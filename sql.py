50ea2012df6626967dff50848a40177a62bcac84
Ariel Fogel <fogeltine@gmail.com>
2018-02-09 14:26:21 -0800

    Run first migration to create db, fix syntax errors from migration script

--- sql.py
+++ sql.py
@@ -3,17 +3,18 @@
 import sqlite3
 
 with sqlite3.connect('blog.db') as connection:
-	# get a cursor object used to excute SQL commands
+	# get a cursor object used to execute SQL commands
 	c = connection.cursor()
 
+	c.execute('DROP TABLE IF EXISTS posts')
 	# create the table
-	c.excute('''
+	c.execute('''
 		CREATE TABLE posts
 		(title TEXT, post TEXT)
 		''')
 
 	# insert dummy data into the table
-	c.excute('INSERT INTO posts VALUES("Good", "I\'m good."')
-	c.excute('INSERT INTO posts VALUES("Well", "I\'m swell."')
-	c.excute('INSERT INTO posts VALUES("Excellent", "I\'m excellent."')
-	c.excute('INSERT INTO posts VALUES("Merp", "I\'m merp."')
+	c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
+	c.execute('INSERT INTO posts VALUES("Well", "I\'m swell.")')
+	c.execute('INSERT INTO posts VALUES("Excellent", "I\'m excellent.")')
+	c.execute('INSERT INTO posts VALUES("Merp", "I\'m merp.")')

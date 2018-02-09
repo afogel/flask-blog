de7591a11674028b6b230869e52530655a87ad9f
Ariel Fogel <fogeltine@gmail.com>
2018-02-09 14:26:57 -0800

    display blog posts on main page

--- blog.py
+++ blog.py
@@ -43,7 +43,11 @@ def logout():
 @app.route('/main')
 @login_required
 def main():
-	return render_template('main.html')
+	g.db = connect_db()
+	cursor = g.db.execute('SELECT * FROM posts')
+	posts = [dict(title=row[0], post=row[1]) for row in cursor.fetchall()]
+	g.db.close()
+	return render_template('main.html', posts=posts)
 
 if __name__ == '__main__':
 	app.run(debug=True)
